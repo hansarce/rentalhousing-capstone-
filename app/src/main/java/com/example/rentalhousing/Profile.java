c3031bfe53a71465caaa4dfcb6b45843765fb9c6
Hans Stephen Arce <ejarce.HA@gmail.com>
2024-05-27 03:08:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rentalhousing/Profile.java
@@ -0,0 +1,15 @@
+package com.example.rentalhousing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Profile extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
